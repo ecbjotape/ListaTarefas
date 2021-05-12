0a91e66a66f1394573076bad0e73779cf852c62c
unknown <jpmoreiradejesus@hotmail.com>
2021-05-12 08:52:48 -0300

    stylizing tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,17 @@ import './Main.css';
 
 // Form
 import { FaBeer } from 'react-icons/fa';
+// Form
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 export default class Main extends Component{
 state = {
       novaTarefa: '',
+      tarefas: [
+      'Beber café',
+      'Beber água',
+      'Estudar',
+    ],
     };
 
   handleChange = (e) =>{
@@ -17,7 +24,7 @@ state = {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -33,7 +40,19 @@ state = {
             < FaBeer/>
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) =>(
+            <li key={tarefa} className="li">
+              {tarefa}
+              <div>
+                <FaWindowClose className="close" />
+                <FaEdit className="edit" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
